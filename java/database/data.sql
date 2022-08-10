8ed70d9f5550c844029018815524092b0f0f6a0b
Trevor Lee <trevorleeoh@gmail.com>
2022-08-10 11:43:36 -0400

    Added an issue row in database

--- java/database/data.sql
+++ java/database/data.sql
@@ -3,4 +3,7 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+INSERT INTO issues (issue_name, description, user_id, option_a, option_b, option_c)
+VALUES ('Murder', 'What are your thoughts on Murder?', 1, 'Love it', 'Hate it', 'Indifferent');
+
 COMMIT TRANSACTION;
