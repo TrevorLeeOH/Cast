b69806981cfe27bff995caf192a57e418142ec3b
Zachary Anderson <anderson.zac@gmail.com>
2022-08-12 15:53:51 -0400

    updating controllers to add authorization and security to methods

--- java/src/main/java/com/techelevator/controller/IssueController.java
+++ java/src/main/java/com/techelevator/controller/IssueController.java
@@ -11,7 +11,6 @@ import com.techelevator.security.jwt.TokenProvider;
 
 import java.util.List;
 
-//@PreAuthorize("isAuthenticated()") //we don't have to pass in token by using this
 @RestController
 @CrossOrigin
 public class IssueController {
@@ -24,46 +23,55 @@ public class IssueController {
         this.issueDao = issueDao;
     }
 
+    @PreAuthorize("permitAll")
     @GetMapping(path = "/issues")
     public List<IssueOverviewDTO> getAllIssues(){
         return issueDao.getAllIssues();
     };
 
+    @PreAuthorize("permitAll")
     @GetMapping(path = "/issues/user/{user_id}")
     public List<IssueOverviewDTO> getIssuesByUser(@PathVariable int user_id){
         return issueDao.getIssuesByUser(user_id);
     };
 
+    @PreAuthorize("permitAll")
     @GetMapping(path = "/issues/tag/{tag_id}")
     public List<IssueOverviewDTO> getIssuesByTag(@PathVariable int tag_id){
         return issueDao.getIssuesByTag(tag_id);
     };
 
+    @PreAuthorize("permitAll")
     @GetMapping(path = "/issues/id/{issue_id}")
     public IssueDetailsDTO getIssueByIssueId(@PathVariable int issue_id){
         return issueDao.getIssueByIssueId(issue_id);
     };
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
     @PostMapping(path = "/issues/create")
     public void createIssue(Issue issue) {
         issueDao.createIssue(issue);
     };
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')") //need to add Principal principal to jdbctemplate to specify user that created issue only
     @PutMapping(path = "/issues/issue-name/{issue_name}")
     public void updateIssueByName(Issue updatedIssue) {
         issueDao.updateIssueByName(updatedIssue);
     };
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')") //need to add Principal principal to jdbctemplate to specify user that created issue only
     @PutMapping(path = "/issues/issue-id/{issue_id}")
-    public void updateIssueById(Issue updatedIssue) { //should this be a string or int? may need to change model/jdbcdao
+    public void updateIssueById(Issue updatedIssue) {
         issueDao.updateIssueById(updatedIssue);
     };
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')") //need to add Principal principal to jdbctemplate to specify user that created issue only
     @RequestMapping(path = "/issues/delete-name/{issue_name}", method = RequestMethod.DELETE)
     public void deleteIssueByName(String name) {
         issueDao.deleteIssueByName(name);
     };
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')") //need to add Principal principal to jdbctemplate to specify user that created issue only
     @RequestMapping(path = "/issues/delete-id/{issue_id}", method = RequestMethod.DELETE)
     public void deleteIssueById(int issueId) {
         issueDao.deleteIssueById(issueId);
