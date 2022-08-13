180bde371a6b23341f455a6a6a31d697baa617f8
Matt Dean <mdaviddean@gmail.com>
2022-08-13 15:21:44 -0400

    Adding issues for content

--- java/database/data.sql
+++ java/database/data.sql
@@ -3,13 +3,29 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
-INSERT INTO issues (issue_name, description, user_id, option_a, option_b, option_c)
-VALUES ('Murder', 'What are your thoughts on Murder?', 1, 'Love it', 'Hate it', 'Indifferent');
-
 INSERT INTO issues (issue_name, description, user_id, option_a, option_b, option_c, option_d)
 VALUES ('Why You Vote', 'Rank these issues on their importance in determining your vote in the upcoming election', 1, 'Environment', 'Health Care', 'Economy', 'Foreign Relations');
 
 INSERT INTO issues (issue_name, description, user_id, option_a, option_b, option_c, option_d, option_e, option_f, option_g, option_h)
 VALUES ('Political Labels', 'Rank these labels from most positive association to least', 1, 'Moderate', 'Liberal', 'Conservative', 'Centrist', 'Progressive', 'Republican', 'Democrat', 'Independent');
+
+INSERT INTO issues (issue_name, description, user_id, option_a, option_b, option_c, option_d, option_e)
+VALUES ('Democratic Primary Candidates', 'If Joe Biden decided not to run for reelection and the Democratic primary were held today, rank these choices from most likely to least likely to have your support:', 1, 'Kamala Harris', 'Bernie Sanders', 'Corey Booker', 'Amy Klobuchar', 'Pete Buttigieg');
+
+INSERT INTO issues (issue_name, description, user_id, option_a, option_b, option_c, option_d, option_e)
+VALUES ('Republican Primary Candidates', 'If the Republican presidential primary were held today, rank these choices from most likely to least likely to have your support:', 1, 'Donald Trump', 'Mike Pence', 'Ted Cruz', 'Nikki Haley', 'Ron DeSantis');
+
+INSERT INTO issues (issue_name, description, user_id, option_a, option_b, option_c, option_d, option_e)
+VALUES ('Third Parties', 'Rank these third parties based on how much you know about them from most to least:', 1, 'Libertarian Party', 'Green Party', 'Constitutional Party', 'Rent Is Too Damn High Party', 'Socialist Party USA');
+
+INSERT INTO issues (issue_name, description, user_id, option_a, option_b, option_c, option_d, option_e)
+VALUES ('Gun Violence Policy Solutions', 'Rank from most preferred to least preferred these policy solutions to gun violence:', 1, 'Do Nothing', 'Incentivize Increased Gun Ownership for Self Protection', 'Minimal Increases in Firearm Regulations i.e Background Checks', 'Assault Weapons Ban', 'Full Firearm Ban');
+
+INSERT INTO issues (issue_name, description, user_id, option_a, option_b, option_c, option_d)
+VALUES ('Health Care Systems', 'Rank from most preferred to least preferred these forms of health care systems', 1, 'Universal Government Funded Health Care', 'Fully Privatized Health Insurance', 'Government Assistance only for Those Who Meet Strict Requirements', 'Employer Provided Health Insurance');
+
+INSERT INTO issues (issue_name, description, user_id, option_a, option_b, option_c, option_d, option_e)
+VALUES ('Environmental Impacts', 'Rank these issues by how much they impact your community from most impactful to least impactful', 1, 'Water Contamination', 'Air Quality', 'CLimate Change', 'Sustainable Energy', 'Waste Disposal');
+
 
 COMMIT TRANSACTION;
--- vue/src/components/MainHeader.vue
+++ vue/src/components/MainHeader.vue
@@ -1,19 +1,88 @@
 <template>
-  <div>
-      <h2>Cast✓</h2>
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'active-issues' }">Vote Now!</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'closed-issues' }">Closed Polls</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'register' }">Create Account</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'login' }" v-if="$store.state.token = 'true'">Login</router-link>
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != 'true'">Logout</router-link> &nbsp;|&nbsp;
+
+  
+  <div class="navbar" id="myNavBar">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <a><img src="C:\Users\Student\workspace\java-yellow-finalcapstone-team3\vue\src\assets\CAST_logo-02.png"/></a>
+      <router-link tag="li" v-bind:to="{ name: 'home' }">Home</router-link>
+      <router-link tag="li" v-bind:to="{ name: 'active-issues' }">Vote Now!</router-link>
+      <router-link tag="li" v-bind:to="{ name: 'closed-issues' }">Closed Polls</router-link>
+      <router-link tag="li" v-bind:to="{ name: 'register' }">Create Account</router-link>
+      <router-link tag="li" v-bind:to="{ name: 'login' }" v-if="$store.state.token = 'true'">Login</router-link>
+      <router-link tag="li" v-bind:to="{ name: 'logout' }" v-if="$store.state.token != 'true'">Logout</router-link>
+   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
   </div>
 </template>
 
 <script>
+export default {
 
+
+methods: { 
+  myFunction() {
+  var x = document.getElementById("myNavbar");
+  if (x.className === "navbar") {
+    x.className += "responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
+}
+}
 </script>
 
 <style>
+
+.navbar {
+  background-color: rgb(32, 86, 187);
+  overflow: hidden;
+}
+
+.navbar a{
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.navbar li{
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+.navbar .icon {
+  display: none;
+}
+
+@media screen and (max-width: 600px) {
+  .navbar li {display: none;}
+  .navbar .icon {
+    float: right;
+    display: block;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .navbar.responsive {position: relative;}
+  .navbar.responsive li.icon {
+    position: absolute;
+    right: 0;
+    top: 0;
+  }
+  .navbar.responsive li {
+    float: none;
+    display: block;
+    text-align: left;
+  }
+}
 
 </style>
